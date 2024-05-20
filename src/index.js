1aba782f24742a50119733a7d7133de43059e115
Maksim Burtsev <maks.artmovich@bk.ru>
2024-05-20 23:01:49 +0300

    test of tests

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const capitalize = text => {
+    if(text === '') return ''
+	const firstChar = text[0].toUpperCase()
+	const restSubstring = text.slice(1)
+	return `${firstChar}${restSubstring}`
+}
+
+export default capitalize
