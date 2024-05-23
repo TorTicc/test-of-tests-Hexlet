7942ba84c09b803fc43449a8c577076c00484f74
Maksim Burtsev <maks.artmovich@bk.ru>
2024-05-23 23:01:20 +0300

    test from asserts

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,11 +1,7 @@
+import { strict as assert } from 'node:assert'
 import capitalize from '../src/index.js'
 
-if (capitalize('hello') !== 'Hello') {
-	throw new Error('Функция работает неверно!')
-}
-
-if (capitalize('') !== '') {
-	throw new Error('Функция работает неверно!')
-}
+assert.equal(capitalize('hello'), 'Hello')
+assert.equal(capitalize(''), '')
 
 console.log('Все тесты пройдены!')
